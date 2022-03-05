ca7257cc856ba07acc33ece659d392a9b5b9b58d
Alexander Powell <ajpowell@email.wm.edu>
2022-03-04 21:20:04 -0500

    feb updates

--- app.py
+++ app.py
@@ -34,6 +34,10 @@ DESCENDING = firestore.Query.DESCENDING
 ASCENDING = firestore.Query.ASCENDING
 
 UIDS = [('0tPGnN8kdwP8TfMPInmulOfxJnF2', -8), # progread@soboba.net
+		('0uBA0S1nqXfMs2YYnoYdREVoM2Q2', -8), # sevans@peppermillreno.com
+		('69yPfdP277dZDAMP4TIpyb3ZSwx2', -8), # rhsvc@rollinghillscasino.com
+		('Q86qgiOu6UcrRF97OHmYeIyCT6a2', -8), # tachipalace@mic.com
+		('rLhBmUAqIBXUNthKbWrlSaiyn832', -8), # mtrread@spc.com
 		('HsO6si6GalMnSxYhl5Y5yBHIV5n2', -8), # wrphoto@wendoverfun.com
 		('LMaZ7oaPxXXVAiKnneBg8Eqkw912', -8), # wpphoto@wendoverfun.com
 		('M8ABeaNkhDWKl7yloZnRDFVEWoy1', -8), # wmphoto@wendoverfun.com
@@ -47,23 +51,48 @@ UIDS = [('0tPGnN8kdwP8TfMPInmulOfxJnF2', -8), # progread@soboba.net
 		('Oko1D0siR7Q7NneCOCe0tLdRw7J3', -8), # acpsmeters@srcmail.net
 		('ZXOj33tfp6SrZ7QB0txYL1toAIF3', -8), # acccmeters@accmail.net
 		('uJjfFFwtnyL8YKFMBATkJW57BNZ2', -8), # mic@gratonresortcasino.com
+		('WZ2c9X1QCzRkgE0HYGKgAHIVYQC3', -8), # rhsvcmic@rollinghillscasino.com
+		('2gR3TX7zR9P6KwTjffPmizw0MF23', -8), # chickenranch@mic.com
+		('Cc8hmrfnMaRizETqyCqSgxll9n62', -8), # stlvprogressivemeter@boydgaming.com
+		('sznI4hVPWcP6h3dw4LuLYgNzpJK2', -8), # azcwmeters@goldenent.com
+		('W8c5VOXRWHMdqMkwLfVxWxkxz393', -8), # vvmeters@mic.com
+		('e0Y8noCWQNX8w4iBzRi5WitePAz1', -6), # prairiebandmeters@mic.com
 
-		### For testing ###
+		('BBPHTXVe53XZPv0HD3mLnUBdEMF3', -8), # g2e@everi.com
+		('lOayiEsclAchyOUSrGotmoldKl42', -8), # eaglemountainmeters@mic.com
+		('SN0GLxWln7Uiwc4ay8YeJ4Eu1zg2', -8), # jeffrey.hoss@everi.com
+		('D4vMsOCN9rP1V2n3O91gikqpPY22', -5), # fwd@mic.com
+		('sPFTqKf0ygdPpWGPyGeYEbwM91t1', -5), # fwh@mic.com
+		('Isadzx74ThSU4FQwoO2ftivoZq23', -5), # fwnb@mic.com
+		('pOKeHHjgRRPeWVUzWMvsrzcBddk2', -5), # fwsb@mic.com
+		('eUobdthlVRcW84qLgBdVOPsSZwl2', -5), # fourwindsmeters@mic.com
+		('bKy7OVZernbqUZB2Ht14vRGQR5g1', -5), # earthcasinometers@mic.com
+		('2Y9yQbUpOnMQuEeLghoPEBu4orp1', -5), # skycasinometers@mic.com
+		('eOM7mteC6gZ9yNlim5fWx08bmOq1', -5), # mohegansunmeters@mic.com
+		('AXWCFEoN2ZSUiglB6DNJjHugwOg1', -8), # harrahssocal@mic.com
+		('1LQMQ06VspVCs70ljzGBPIHHPgr1', -5), # lhmeters@mic.com
+		('sygjkUWRD1bfpfQM8QQuzwNGQXx1', -5), # ybrmeters@mic.com
+		('gK4CbuX1Jwa2ExZI0mmXRGnReZ03', -5), # ppmeters@mic.com
+		('VX4QGsBEsAd5vPaNRfabmmGrA4f2', -5), # tsmeters@mic.com
+		('yGHCFZbiZweOvblSGL1LOH9x4lf2', -5), # oneidameters@mic.com
+		('by6NFwVisrVESJB04hyEBtZRgT03', -8), # timeters@mic.com
+		('UIOUZogcnFPFH8S0j7RvLLuodkh1', -8), # autotest@mic.com
+		('P14IjeLFv3Oyx4t2SHaG9IYULEu1', -8), # Christine.foster@everi.com
+		('b8UzXzJGhQY6LtTuUPJNG3Y8ShW2', -8), # Subhasis.pradhan@everi.com
+		('7rgeKrCE2NhjAWuZVcgVDwwOBoO2', -8), # paumameters@mic.com
+
+
+
+		# ### For testing ###
 		('1kyN8HCbC6gfZY8nNIYB1HjqRnH3', -8), # joe@capturemeters.com
 		('xgdRnVu3yrgjEhrMQgDSImBEOCc2', -5)] # lotrrox@gmail.com
 
-monthName = 'february'
-
-'''startDate = datetime.datetime(2020, 11, 29, 17, 00, tzinfo=timezone.utc)
-endDate = datetime.datetime(2021, 1, 1, 0, 0, tzinfo=timezone.utc)
-lastDayOfMonth = datetime.datetime(2020, 12, 31, 17, 00, tzinfo=timezone.utc)'''
-# startDate = datetime.datetime(2020, 12, 31, 17, 00, tzinfo=timezone.utc)
-# endDate = datetime.datetime(2021, 2, 1, 0, 0, tzinfo=timezone.utc)
-# lastDayOfMonth = datetime.datetime(2021, 1, 31, 17, 00, tzinfo=timezone.utc)
+monthName = 'february2022'
+yearName = '2022'
 
 # Reset time is 5pm local
-startDate = datetime.datetime(2021, 1, 30, 17, 0, tzinfo=timezone.utc)
-endDate = datetime.datetime(2021, 3, 1, 17, 0, tzinfo=timezone.utc)
+startDate = datetime.datetime(2022, 1, 31, 17, 0, tzinfo=timezone.utc)
+endDate = datetime.datetime(2022, 3, 1, 17, 0, tzinfo=timezone.utc)
 
 machineIds = set()
 
@@ -157,11 +186,21 @@ for item in UIDS:
 				if not isAllNone(row):
 					writer.writerow(formatRow(row)[:headerLength])
 
-	blob = bucket.blob(uid + '/february2021.csv')
+	blob = bucket.blob(uid + '/' + monthName + '.csv')
 	blob.upload_from_filename('/Users/alexanderpowell/Desktop/projects/mic-monthly-report-generator/' + fileName)
 	print('Report created for uid: ' + uid)
 
 print(str(counter) + ' rows fetched in total')
+
+
+
+
+
+
+
+
+
+
 
 
 
